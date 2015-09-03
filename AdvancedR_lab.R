952d0f2bce278ef14846838b340433b56c1e3b13
Araya Eamrurksiri <araea382@student.liu.se>
2015-09-03 19:39:25 +0200

    add assertion

--- AdvancedR_lab.R
+++ AdvancedR_lab.R
@@ -1,9 +1,14 @@
-#
-
 euclidian<-function(a, b){
-while (b != 0){
-t <- b
-b <- a %% b
-a <- t}
-return(a)
+    #assertion
+    stopifnot(length(a) <= 1)
+    stopifnot(length(b) <= 1)
+    stopifnot(is.numeric(a) & is.numeric(b))
+    
+    #implementation
+    while (b != 0){
+        t <- b
+        b <- a %% b
+        a <- t
+    }
+    return(a)
 }
