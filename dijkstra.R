3323f0e41151adbce76120d48841aa4405b2bbe2
Araya Eamrurksiri <araea382@student.liu.se>
2015-09-04 18:18:14 +0200

    correct the spelling

--- dijkstra.R
+++ dijkstra.R
@@ -3,7 +3,7 @@ dijkstra <- function(graph, init_node){
     stopifnot(length(init_node) <= 1)
     stopifnot(is.numeric(init_node))
     for(i in 1:2){
-        stopifnot(any(wiki_graph[i] == init_node))
+        stopifnot(any(graph[i] == init_node))
     }
     distance<-NULL
     #maybe not order
