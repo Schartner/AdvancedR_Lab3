d63d11a0ec169c5dc40dc738c9942e47c2fe02a3
unknown <Clara.Schartner@gmail.com>
2015-09-06 21:23:57 +0200

    Some tests added

--- AdvancedRLab/R/dijkstra.R
+++ AdvancedRLab/R/dijkstra.R
@@ -1,62 +1,67 @@
-dijkstra <- function(graph, init_node){#
-  #assertion
-  stopifnot(length(init_node) <= 1)
-  stopifnot(is.numeric(init_node))
-  for(i in 1:2){
-    stopifnot(any(graph[i] == init_node))
-  }
-  
-  
-  #  distance<-NULL
-  distance[all.nodes]<-Inf
-  distance[init_node]<-0
-  #maybe not order
-  all.nodes<-sort(unique(graph[[1]]))
-  names(distance)<-all.nodes
-  
-  
-  visit<-init_node
-  
-  #for (i in v1){
-  #  if (i!=init_node){
-  #   distance[i]<- Inf
-  #}
-  
-  #  }
-  #implementation
-  
-  visited<-c(init_node)
-  
-  #  for (i in 1:length(unique(graph[[3]]))){
-  visit[1]<-init_node
-  distance[graph[2][graph[1]==init_node]]<-graph[[3]][graph[1]==init_node]
-  next.node<-function(distance,visit)
-  {if(any(is.na(visit))){visit[is.na(visit)]<-0}
-    as.numeric(names(which.min(distance[names(distance)!=visit])))
-  }
-  
-  
-  i<-next.node(distance,visit)
-  visit[next.node(distance,visit)]<-i
-  
-  while (!all(all.nodes %in% visit)){
+#'Dijkstra Algorithm
+#'
+#'A method for finding the shortest path between the nodes in a graph.
+#'
+#'@param graph a dataframe consists of three variables 
+#'@param init_node a numeric scalar for the first node which also exist in the graph
+#'
+#'@return The value is a vector. It is the least value (or shortest path) from the starting node to every nodes in a graph.
+#'
+#'@references \url{https://en.wikipedia.org/wiki/Dijkstra}
+#'
+#'@examples
+#'dijkstra(wiki_graph, 1) #0 7 9 20 20 11
+#'dijkstra(wiki_graph, 3) #9 10 0 11 11 2
+#'
+
+dijkstra <- function(graph, init_node){
+    #assertion for graph
+    stopifnot(is.data.frame(graph))
+    stopifnot(length(graph) == 3)
+
     
-    temporary<-c(rep(Inf,length(all.nodes)))
-    temporary[graph[2][graph[1]==i]]<-graph[3][graph[1]==i]
+    #assertion for init_node
+    stopifnot(length(init_node) <= 1)
+    stopifnot(is.numeric(init_node))
+    for(i in 1:2){
+        stopifnot(any(graph[i] == init_node))
+    }
     
-    distance[(temporary+distance[i])<distance]<-(temporary+distance[i])[(temporary+distance[i])<distance]
+    #define variables
+    distance <- numeric(0)
+    visit <- numeric(nrow(unique(graph[1])))
     
-    #determine next one
-    i<-next.node(distance,visit)
-    visit[next.node(distance,visit)]<-i
-  }
-  distance
-} 
+    ##all.nodes should be declare before this command line 'distance[all.nodes]>-Inf'
+    all.nodes <- sort(unique(graph[[1]]))
+    distance[all.nodes] <- Inf    
+    names(distance) <- all.nodes
+    distance[init_node] <- 0
+ 
+    #the vector visit that keeps track of all the nodes that have been visited
+    visit[init_node] <- init_node
 
-
-
-test_frame<-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-                       v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-                       w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-
-dijkstra(test_frame,1)
+    #calculate distances from initial point. 
+    ##might be possible to implement in while loop. will try later
+    distance[graph[2][graph[1] == init_node]] <- graph[[3]][graph[1] == init_node]
+    
+    #function that determines the next node taking in account the distances already gone and points already visited
+    next.node <- function(distance, visit){
+        return(as.numeric(names(which.min(distance[names(distance)!=visit]))))
+    }
+  
+    #if not all points have been visited, the while loop will loop for the distances 
+    while(!all(all.nodes %in% visit)){
+        #determine the next node
+        i <- next.node(distance, visit)
+        visit[next.node(distance, visit)] <- i
+        
+        #temporary has the distances from current node to all neighboring
+        temporary <- c(rep(Inf,length(all.nodes)))
+        temporary[graph[2][graph[1] == i]] <- graph[3][graph[1] == i]
+        
+        temp <- temporary + distance[i]
+        distance[temp < distance] <- temp[temp < distance]
+    }
+    names(distance) <- NULL
+    return(distance)
+}
--- AdvancedRLab/tests/testthat/test-return_value.R
+++ AdvancedRLab/tests/testthat/test-return_value.R
@@ -12,6 +12,15 @@ test_that("check the return value if it's a numeric vector",{
     expect_is(dijkstra(graph, i), "numeric")
 })
 
+#this test is not necessarily true for another dataset.  there could be a node not connected to inital. will write stopifnot
 test_that("check the return value if it has an infinity",{
     expect_equal(any(dijkstra(graph,i) == Inf), FALSE)
+})
+
+test_that("check the return distance of initial node is 0",{
+  expect_equal(dijkstra(graph,i)[i] == 0, TRUE)
+})
+
+test_that("The distance is higher than all possible connectiones combined. At least one connection used twice",{
+  expect_equal(all(dijkstra(graph,i) < sum(graph[3])), TRUE)
 })
