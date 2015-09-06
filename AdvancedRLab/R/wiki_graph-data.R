3f667703edf8d5f02829c6c38db38e2333d91a3c
unknown <Clara.Schartner@gmail.com>
2015-09-06 20:14:22 +0200

    commit

---
+++ AdvancedRLab/R/wiki_graph-data.R
@@ -0,0 +1,24 @@
+#'Graph
+#'
+#'A dataset containing the nodes and the length of paths.
+#'
+#'@name wiki_graph
+#'@docType data
+#'
+#'@usage data(wiki_graph)
+#'
+#'@format A data frame with 18 rows and 3 variables:
+#'\describe{
+#'  \item{v1}{from: the edges of the graph}
+#'  \item{v2}{to: the edges of the graph}
+#'  \item{w}{weight of the edge}
+#'}
+#'
+#'@keywords datasets
+#'
+#'@references \url{https://en.wikipedia.org/wiki/Dijkstra}
+#'
+#'@examples
+#'data(wiki_graph)
+#'
+NULL
