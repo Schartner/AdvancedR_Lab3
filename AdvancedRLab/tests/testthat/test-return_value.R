5c11e64c5113626ff45b46d384f5ddd12d12df6a
unknown <Clara.Schartner@gmail.com>
2015-09-06 22:35:47 +0200

    corrected mistake: has to be half of the sum since every distance is specified twice

--- AdvancedRLab/tests/testthat/test-return_value.R
+++ AdvancedRLab/tests/testthat/test-return_value.R
@@ -12,15 +12,10 @@ test_that("check the return value if it's a numeric vector",{
     expect_is(dijkstra(graph, i), "numeric")
 })
 
-#this test is not necessarily true for another dataset.  there could be a node not connected to inital. will write stopifnot
-test_that("check the return value if it has an infinity",{
-    expect_equal(any(dijkstra(graph,i) == Inf), FALSE)
-})
-
 test_that("check the return distance of initial node is 0",{
   expect_equal(dijkstra(graph,i)[i] == 0, TRUE)
 })
 
-test_that("The distance is higher than all possible connectiones combined. At least one connection used twice",{
-  expect_equal(all(dijkstra(graph,i) < sum(graph[3])), TRUE)
+test_that("Distance not possible. Has to be either Infinity (not connected) or smaller than total sum of distances",{
+  expect_equal(all(dijkstra(graph,i) < sum(graph[3]/2)|dijkstra(graph,i)==Inf), TRUE)
 })
