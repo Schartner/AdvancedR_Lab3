bebd4db57bbe2e39da61c346d01eaff80ec957c1
Araya Eamrurksiri <araea382@student.liu.se>
2015-09-09 11:06:21 +0200

    delete this test

--- AdvancedRLab/tests/testthat/test-zero_number.R
+++
@@ -1,9 +0,0 @@
-context("zero number")
-
-test_that("when either of the number is zero, it give another number back",{
-    a <- sample(1:100,1)
-    b <- sample(1:100,1)
-    
-    expect_equal(euclidian(0,b), b)
-    expect_equal(euclidian(a,0), a)
-})
